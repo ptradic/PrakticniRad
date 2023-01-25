16e2ef79f3cda2ba4da8bb4056b52c994129713b
bpuizi00 <92427804+bpuizi00@users.noreply.github.com>
2023-01-25 19:16:18 +0100

    test

---
+++ Project36/Project36/LogIn.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define MAX_SIZE 50
+
+typedef struct _BinarySearchTree* Position;
+typedef struct _BinarySearchTree {
+	char username[MAX_SIZE];
+	char password[MAX_SIZE];
+	Position left;
+	Position right;
+}BinarySearchTree;
+
+int checkUsernameSize(char* username, int size);
+int LoadAccountsFromFile(Position head);
+int AddAccountToFile(Position head);
+Position AllocateMemory(Position current);
+int FindElementByUser(Position current, char* username);
+Position LogIn(Position head);
+Position SortedInsert(Position current, Position newElement);
+int INorderPrint(Position current);
+int AddNewAccountToList(Position head, char* username, char* password);
+Position FindLoginUser(Position current, char* username);
+
+
